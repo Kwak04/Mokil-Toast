4b91913f7b639f5dff6b12c7974c8e22c2110f08
Kwak04 <tronixdevelop@gmail.com>
2019-12-01 21:12:54 +0900

    feat: Change title when the tabs are changed

--- app/src/main/java/com/example/mokil_toast/MainActivity.java
+++ app/src/main/java/com/example/mokil_toast/MainActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TabHost;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView title;
 
     TabHost tabHost;
 
@@ -15,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        title = findViewById(R.id.tv_title);
         tabHost = findViewById(R.id.tab_host);
 
         getWindow().setStatusBarColor(getResources().getColor(R.color.colorBackground));
@@ -37,5 +41,23 @@ public class MainActivity extends AppCompatActivity {
         classTabImage.setImageResource(R.drawable.class_tab_button);
         classTab.setIndicator(classTabImage);
         tabHost.addTab(classTab);
+
+        // Change title when the tabs are changed
+        tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+                int tabIndex = tabHost.getCurrentTab();
+
+                // Battle tab
+                if (tabIndex == 0) {
+                    title.setText(R.string.text_battle);
+                }
+
+                // Class tab
+                if (tabIndex == 1) {
+                    title.setText(R.string.text_class);
+                }
+            }
+        });
     }
 }
