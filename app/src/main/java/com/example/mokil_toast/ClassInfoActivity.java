0b24da3ee6c567ab5ec6551a44da5d236733f657
Kwak04 <tronixdevelop@gmail.com>
2019-12-03 23:16:59 +0900

    feat: class number title

--- app/src/main/java/com/example/mokil_toast/ClassInfoActivity.java
+++ app/src/main/java/com/example/mokil_toast/ClassInfoActivity.java
@@ -2,13 +2,29 @@ package com.example.mokil_toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Objects;
 
 public class ClassInfoActivity extends AppCompatActivity {
+
+    TextView classNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class_info);
+
+        getWindow().setStatusBarColor(getResources().getColor(R.color.colorBackground));
+
+        classNumber = findViewById(R.id.tv_class_number);
+
+
+        // Title
+        int classNumberValue = Objects.requireNonNull(getIntent().getExtras()).getInt("classNumber");
+        String classNumberText = classNumberValue + "반";
+        classNumber.setText(classNumberText);
     }
 }
