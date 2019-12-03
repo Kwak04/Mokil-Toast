9cf49b93bf22a982719771a09095e74c5a9c2016
Kwak04 <tronixdevelop@gmail.com>
2019-12-04 01:31:54 +0900

    feat: win rate in main panel

--- app/src/main/java/com/example/mokil_toast/ClassInfoActivity.java
+++ app/src/main/java/com/example/mokil_toast/ClassInfoActivity.java
@@ -10,7 +10,7 @@ import java.util.Objects;
 
 public class ClassInfoActivity extends AppCompatActivity {
 
-    TextView classNumber;
+    TextView classNumber, win, voteRate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +20,23 @@ public class ClassInfoActivity extends AppCompatActivity {
         getWindow().setStatusBarColor(getResources().getColor(R.color.colorBackground));
 
         classNumber = findViewById(R.id.tv_class_number);
+        win = findViewById(R.id.tv_win);
+        voteRate = findViewById(R.id.tv_vote_rate);
 
+
+        // getIntent
+        int classNumberValue = Objects.requireNonNull(getIntent().getExtras()).getInt("classNumber");
+        int winValue = getIntent().getExtras().getInt("win");
 
         // Title
-        int classNumberValue = Objects.requireNonNull(getIntent().getExtras()).getInt("classNumber");
         String classNumberText = classNumberValue + "반";
         classNumber.setText(classNumberText);
+
+        // Main
+        String winText = winValue + "회";
+        win.setText(winText);
+
+        // Battle list
+
     }
 }
