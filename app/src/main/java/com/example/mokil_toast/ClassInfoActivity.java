04aead801406bc243e6047decebdd54f89c76a00
Kwak04 <tronixdevelop@gmail.com>
2019-12-03 23:05:21 +0900

    feat: Go to ClassInfoActivity when pressed class list

---
+++ app/src/main/java/com/example/mokil_toast/ClassInfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.mokil_toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClassInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class_info);
+    }
+}
--- app/src/main/java/com/example/mokil_toast/ClassListAdapter.java
+++ app/src/main/java/com/example/mokil_toast/ClassListAdapter.java
@@ -1,5 +1,7 @@
 package com.example.mokil_toast;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,9 +12,36 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class ClassListAdapter extends RecyclerView.Adapter<ClassListAdapter.ViewHolder> {
     private ClassData classData;
+    private OnItemTouchListener listener;
 
     ClassListAdapter(ClassData classData) {
         this.classData = classData;
+    }
+
+    public void setOnItemTouchListener(OnItemTouchListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ClassListAdapter.ViewHolder holder, final int position) {
+        String classText = classData.results[position].class_number + "반";
+        String winText = "이긴 횟수: " + classData.results[position].win;
+        holder.classNumber.setText(classText);
+        holder.win.setText(winText);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), ClassInfoActivity.class);
+                int classNumberValue = Integer.parseInt(classData.results[position].class_number);
+                int winValue = Integer.parseInt(classData.results[position].win);
+
+                intent.putExtra("classNumber", classNumberValue);
+                intent.putExtra("win", winValue);
+
+                view.getContext().startActivity(intent);
+            }
+        });
     }
 
     @NonNull
@@ -23,12 +52,8 @@ public class ClassListAdapter extends RecyclerView.Adapter<ClassListAdapter.View
         return new ViewHolder(v);
     }
 
-    @Override
-    public void onBindViewHolder(@NonNull ClassListAdapter.ViewHolder holder, int position) {
-        String classText = classData.results[position].class_number + "반";
-        String winText = "이긴 횟수: " + classData.results[position].win;
-        holder.classNumber.setText(classText);
-        holder.win.setText(winText);
+    public interface OnItemTouchListener {
+        void onItemClick(View v, int position);
     }
 
     @Override
@@ -39,7 +64,7 @@ public class ClassListAdapter extends RecyclerView.Adapter<ClassListAdapter.View
     class ViewHolder extends RecyclerView.ViewHolder {
         TextView classNumber, win;
 
-        ViewHolder(@NonNull View itemView) {
+        ViewHolder(@NonNull final View itemView) {
             super(itemView);
 
             classNumber = itemView.findViewById(R.id.tv_class);
